e53361e10736c960036a0f63f2d432b4b6a24d77
suziLe <55001885+suziLe@users.noreply.github.com>
2019-11-25 01:38:18 -0800

    ArmorBase.java class
    added ArmorBase.java class

---
+++ src/main/java/com/CSCI441Minecraft/questMod/items/armour/ArmorBase.java
@@ -0,0 +1,28 @@
+package com.CSCI441Minecraft.questMod.items.armor;
+
+import com.CSCI441Minecraft.questMod.Main;
+import com.CSCI441Minecraft.questMod.ModItems;
+import com.CSCI441Minecraft.questMod.Model;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+
+public class ArmorBase extends ItemArmor implements Model {
+
+	public ArmorBase(String name, ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
+		super(materialIn, renderIndexIn, equipmentSlotIn);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.COMBAT);
+		
+		ModItems.ITEMS.add(this);
+	}
+	
+	@Override
+	public void registerModels() 
+	{
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+
+}
